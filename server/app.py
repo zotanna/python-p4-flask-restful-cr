cfb4a3ddb9c2335b46dd89c3bef3ba89e4416cb8
denis <denniiskim@gmail.com>
2024-03-08 03:32:28 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-from flask import Flask, request, make_response
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -16,8 +14,72 @@ db.init_app(app)
 
 api = Api(app)
 
-class Home(Resource):
-    pass
+class Index(Resource):
+
+    def get(self):
+
+        response_dict = {
+            "index": "Welcome to the Newsletter RESTful API",
+        }
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+
+        return response
+
+api.add_resource(Index, '/')
+
+class Newsletters(Resource):
+
+    def get(self):
+
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+
+        response = make_response(
+            jsonify(response_dict_list),
+            200,
+        )
+
+        return response
+
+    def post(self):
+
+        new_record = Newsletter(
+            title=request.form['title'],
+            body=request.form['body'],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            201,
+        )
+
+        return response
+
+api.add_resource(Newsletters, '/newsletters')
+
+class NewsletterByID(Resource):
+
+    def get(self, id):
+
+        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+
+        return response
+
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
+
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
